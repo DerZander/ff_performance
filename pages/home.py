04fa4ddc2c6fc93a5c813d9d56d3ec3eb5ae1ff3
Timo Zander <timo.zander@outlook.com>
2025-07-26 12:56:02 +0200

    changed the homepage

--- pages/home.py
+++ pages/home.py
@@ -2,11 +2,13 @@ import streamlit as st
 
 
 def home_index():
-    st.title("Home Page")
+    st.title("Kreisfeuerwehrtag 2025")
+    st.write("Willkommen auf der Hilfsseite für den Kreisfeuerwehrtag 2025!")
+    st.write("Auf dieser Seite finden Sie alle wichtigen Informationen und Ressourcen, die Sie benötigen, um sich auf den Tag vorzubereiten.")
 
 
 page_home = st.Page(
     home_index,
-    title="Home Page",
+    title="Home",
     icon="🏠"
 )
